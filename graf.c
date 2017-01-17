a0e595924ee2c8ea664a31df24c19503f0c6af0e
Adi Purnama <adipurnama@s.itb.ac.id>
2017-01-17 14:29:27 +0700

    Refractoring

--- graf.c
+++ graf.c
@@ -1,36 +1,40 @@
- #include <stdlib.h>
- #include <unistd.h>
- #include <stdio.h>
- #include <fcntl.h>
- #include <linux/fb.h>
- #include <sys/mman.h>
- #include <sys/ioctl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
 #include <time.h>
+
+
+
+int fbfd = 0;                       //Filebuffer Filedescriptor
+struct fb_var_screeninfo vinfo;     //Struct Variable Screeninfo
+struct fb_fix_screeninfo finfo;     //Struct Fixed Screeninfo
+long int screensize = 0;            //Ukuran data framebuffer
+char *fbp = 0;                      //Framebuffer di memori internal
+
+
+
+// STRUKTUR DATA BLOCK CHARACTER
+// --------------------------------------------------------------------------------------------------------- //
 
 typedef struct {
   int *array;
   size_t used;
   size_t size;
-} Array;
+  int x;
+  int y;
+} Block;
 
-int fbfd = 0;
-struct fb_var_screeninfo vinfo;
-struct fb_fix_screeninfo finfo;
-long int screensize = 0;
-char *fbp = 0;
-int x = 0, y = 0;
-long int location = 0;
-Array block;
-
-
-
-void initArray(Array *a, size_t initialSize) {
+void initArray(Block *a, size_t initialSize) {
   a->array = (int *)malloc(initialSize * sizeof(int));
   a->used = 0;
   a->size = initialSize;
 }
 
-void insertArray(Array *a, int element) {
+void insertArray(Block *a, int element) {
   // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
   // Therefore a->used can go up to a->size 
   if (a->used == a->size) {
@@ -40,14 +44,24 @@ void insertArray(Array *a, int element) {
   a->array[a->used++] = element;
 }
 
-void freeArray(Array *a) {
+void freeArray(Block *a) {
   free(a->array);
   a->array = NULL;
   a->used = a->size = 0;
 }
 
+void setCoordinate(Block *a , int _x , int _y){
+    a->x = _x;
+    a->y = _y;
+}
 
-void init() {
+// --------------------------------------------------------------------------------------------------------- //
+
+
+
+void initScreen() {
+//Mapping framebuffer ke memori internal
+
      // Buka framebuffer
      fbfd = open("/dev/fb0", O_RDWR);
      if (fbfd == -1) {
@@ -56,25 +70,23 @@ void init() {
      }
      printf("The framebuffer device was opened successfully.\n");
 
-     // Get fixed screen information
+     // Ambil data informasi screen
      if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
          perror("Error reading fixed information");
          exit(2);
      }
-
-     // Get variable screen information
      if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
          perror("Error reading variable information");
          exit(3);
      }
 
-     //print resolusi layar dan jumlah bit per piksel
+     // Informasi resolusi, dan bit per pixel
      printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
 
-     // Hitung 
+     // Hitung ukuran memori untuk menyimpan framebuffer
      screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
 
-     // Map the device to memory
+     // Masukkan framebuffer ke memory, untuk kita ubah-ubah
      fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,
                         fbfd, 0);
      if ((int)fbp == -1) {
@@ -83,84 +95,100 @@ void init() {
      }
      printf("The framebuffer device was mapped to memory successfully.\n");
 
-
-     //init block array
-     initArray(&block, 5);
-
 }
 
+
+void plotPixelRGBA(int _x , int _y , int r , int g , int b, int a) {
+//Plot pixel (x,y) dengan warna (r,g,b,a)
+    long int location = (_x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (_y+vinfo.yoffset) * finfo.line_length;
+    *(fbp + location) = b;        //blue
+    *(fbp + location + 1) = g;    //green
+    *(fbp + location + 2) = r;    //red
+    *(fbp + location + 3) = a;      //
+}
+
+void plotPixelRGB(int _x , int _y , int r , int g , int b) {
+//plot pixel (x,y) dengan warna (r,g,b)
+    long int location = (_x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (_y+vinfo.yoffset) * finfo.line_length;
+    unsigned short int t = r<<11 | g << 5 | b;
+    *((unsigned short int*)(fbp + location)) = t;
+}
 
 
 void clearScreen() {
+//Mewarnai latar belakang screen dengan warna putih
+    int x;
+    int y;
     for (y = 0; y < vinfo.yres - 15 ;y++)
          for (x = 0; x < vinfo.xres  ; x++) {
-
-             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                        (y+vinfo.yoffset) * finfo.line_length;
-
              if (vinfo.bits_per_pixel == 32) {
-                 *(fbp + location) = 255;        //blue
-                 *(fbp + location + 1) = 255;    //green
-                 *(fbp + location + 2) = 255;    //red
-                 *(fbp + location + 3) = 0;      //transparansi
+                 plotPixelRGBA(x,y,255,255,255,0);
              } else  { //asumsi mode 16 bit per piksel
-                 int b = 100;   // blue
-                 int g = 100;   // green
-                 int r = 100;   // red
-                 unsigned short int t = r<<11 | g << 5 | b;
-                 *((unsigned short int*)(fbp + location)) = t;
+                 plotPixelRGB(x,y,255,255,255);
              }
 
          }
-
-
 }
 
 
 
-void loadBlockToMemory(char * blockfile, Array * blockmem) {
+void initBlock(char * blockfile, Block * blockmem , int _x , int _y) {
+//Membaca file block character kedalam memori, tempatkan di koordinaat (x,y) layar
+
     FILE * inputFile;
     int pix;
     char see;
     int stop = 0;
 
+    initArray(blockmem, 5);
     inputFile = fopen(blockfile,"r");
+    (*blockmem).x = _x;
+    (*blockmem).y = _y;
 
     while (!stop) {
         fscanf(inputFile, "%d", &pix);
         insertArray(blockmem, pix);
         see = getc(inputFile);
          if (see == '\n') {
-            insertArray(&block, -1);
+            insertArray(blockmem, -1);
          }
          else if( see == EOF) {
             stop = 1;
          }
     }
+}
 
+int isOverflow(int _x , int _y){
+//Cek apakah kooordinat (x,y) sudah melewati batas layar
+    int result;
+    if ( _x > vinfo.xres ||  _y > vinfo.yres -20 ) {
+        result = 1;
+    }
+    else {
+        result = 0;
+    }
+    return result;
 }
 
 
 
-void drawBlock(Array * blockmem, int xCoord , int yCoord ){
+void drawBlock(Block * blockmem){
+//Menggambar block character pada layar
+
     int i;
-    int xCanvas = xCoord;
-    int yCanvas = yCoord;
+    int xCanvas = blockmem->x;
+    int yCanvas = blockmem->y;
     int pix;
     for (i =0 ; i < (*blockmem).used ; i++) {
         pix = (*blockmem).array[i];
-        //printf("%d ", pix);
         if ( pix == -1 ) {
-            xCanvas = xCoord;
+            xCanvas = blockmem->x;
             yCanvas++;
-            //printf("\n");
         }
         else {
-            location = (xCanvas+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (yCanvas+vinfo.yoffset) * finfo.line_length;
-            *(fbp + location) = pix;        //blue
-            *(fbp + location + 1) = pix;    //green
-            *(fbp + location + 2) = pix;    //red
-            *(fbp + location + 3) = 0;      //transparansi
+            if (!isOverflow(xCanvas,yCanvas)){
+                plotPixelRGBA(xCanvas,yCanvas,pix,pix,pix,0);
+            }
         }
         xCanvas++;
     }
@@ -168,78 +196,61 @@ void drawBlock(Array * blockmem, int xCoord , int yCoord ){
 }
 
 
-void drawText(int xCoord, int yCoord){
+void removeBlock(Block * blockmem){
+//Menghapus block character pada layar
+
     int i;
-    int xCanvas = xCoord;
-    int yCanvas = yCoord;
+    int xCanvas = blockmem->x;
+    int yCanvas = blockmem->y;
     int pix;
-    for (i =0 ; i < block.used ; i++) {
-        pix = block.array[i];
-        //printf("%d ", pix);
+    for (i =0 ; i < (*blockmem).used ; i++) {
+        pix = (*blockmem).array[i];
         if ( pix == -1 ) {
-            xCanvas = xCoord;
+            xCanvas =  blockmem->x;
             yCanvas++;
-            //printf("\n");
         }
         else {
-            location = (xCanvas+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (yCanvas+vinfo.yoffset) * finfo.line_length;
-            *(fbp + location) = pix;        //blue
-            *(fbp + location + 1) = pix;    //green
-            *(fbp + location + 2) = pix;    //red
-            *(fbp + location + 3) = 0;      //transparansi
+            if (!isOverflow(xCanvas,yCanvas)){
+                plotPixelRGBA(xCanvas,yCanvas,255,255,255,0);
+            }
         }
         xCanvas++;
     }
 }
 
-void removeText(int xCoord, int yCoord){
-    int i;
-    int xCanvas = xCoord;
-    int yCanvas = yCoord;
-    int pix;
-    for (i =0 ; i < block.used ; i++) {
-        pix = block.array[i];
-        //printf("%d ", pix);
-        if ( pix == -1 ) {
-            xCanvas = xCoord;
-            yCanvas++;
-            //printf("\n");
-        }
-        else {
-            location = (xCanvas+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (yCanvas+vinfo.yoffset) * finfo.line_length;
-            *(fbp + location) = 255;        //blue
-            *(fbp + location + 1) = 255;    //green
-            *(fbp + location + 2) = 255;    //red
-            *(fbp + location + 3) = 0;      //transparansi
-        }
-        xCanvas++;
+
+void moveUp(Block * blockmem, int px){
+//Animasi gerak ke atas sebesar x pixel
+
+    int j = blockmem -> y;
+    int initPx = j;
+    for (j ; j > initPx - px ; j = j - 4){
+        setCoordinate(blockmem , blockmem->x , j);
+        drawBlock(blockmem);
+        usleep(40000);
+        removeBlock(blockmem);
     }
-
-
+    drawBlock(blockmem);
 }
 
 
- int main()
- {
-     
-     init();
-     clearScreen();
-     loadBlockToMemory("k.txt",&block);   
-
-     int j = 0 ;
-     for ( j = 0 ; j < 300 ; j = j + 2) {
-        removeText(j,j);
-        //drawText(j,j);
-        drawBlock(&block , j , j);
-        usleep(50000);
-        
-     }
-     
-
-     
-
-
+void terminate() {
+//Pengakhiran program.
      munmap(fbp, screensize);
      close(fbfd);
-     return 0;
+}
+
+
+int main()
+{
+    Block block;
+
+    initScreen();
+    clearScreen();
+    initBlock("k.txt",&block, 350, 700);
+    drawBlock(&block);   
+    moveUp(&block, 500); 
+    terminate();
+
+    return 0;
  }
